179dc96eb3839ed564e0f9f1f9a05adf1c23e1bd
Arkadiusz Surma <arkadiusz@uchy.pl>
2017-05-23 17:13:49 +0000

    Remove old .dotfiles

--- home/.screenlayout/normal-3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --off --output DVI-I-1 --mode 1440x900 --pos 0x40 --rotate normal --output DVI-D-1 --mode 1920x1080 --pos 1440x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 3360x0 --rotate normal
